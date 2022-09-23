8d8057d9acb00094964e67ace572513f23c3acb4
Roman Home <t89875041999@yandex.ru>
2022-09-23 17:21:50 +0300

    Changes to be committed: 	modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/components/NavBar.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import { useContext } from "react";
 import { Context } from "..";
-import {Navbar, Container, Nav} from "react-bootstrap";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { SHOP_ROUTE } from "../utils/consts";
-
-
-function NavBar() {
+import { observer } from "mobx-react-lite"
+const NavBar = observer(()=> {
   const { user } = useContext(Context);
 
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <NavLink style={{color: "white", textDecoration: "none"}} to={SHOP_ROUTE} >Покупки</NavLink>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
+        <NavLink
+          style={{ color: "white", textDecoration: "none" }}
+          to={SHOP_ROUTE}
+        >
+          Покупки
+        </NavLink>
+        {user.isAuth ? (
+          <Nav className="ml-auto" style={{color: "white"}}>
+            <Button variant={"outline-light"}>Адмиин панель</Button>
+            <Button variant={"outline-light"} style={{marginLeft: "20px"}} onClick={() => user.setIsAuth(false)}>Выйти</Button>
+          </Nav>
+        ) : (
+          <Nav className="ml-auto" style={{color: "white"}}>
+            <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+          </Nav>
+        )}
       </Container>
     </Navbar>
   );
-}
+})
+
 
 export default NavBar;
